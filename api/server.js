d6d60ef61724cfbe8499cce0b355a23a107c1913
ol222hf <ol222hf@student.lnu.se>
2021-03-18 18:16:40 +0100

    main method with error handling

--- api/server.js
+++ api/server.js
@@ -1,9 +1,52 @@
 import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 
 import * as mongoDB from './config/mongoDB.js'
 import * as mqtt from './config/MQTT.js'
+import { router } from './routes/router.js'
 
 dotenv.config()
 
-mongoDB.connect()
-mqtt.setup()
+const main = async () => {
+    await mongoDB.connect()
+    mqtt.setup()
+
+    const app = express()
+    app.use(cors())
+    app.use(helmet())
+
+    app.use('/', router)
+
+    app.use(function (err, req, res, next) {
+        err.status = err.status || 500
+
+        if (req.app.get('env') !== 'development') {
+            res
+                .status(err.status)
+                .json({
+                    status: err.status,
+                    message: err.message
+                })
+            return
+        }
+
+        return res
+            .status(err.status)
+            .json({
+                status: err.status,
+                message: err.message,
+                innerException: err.innerException,
+                stack: err.stack
+            })
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server started on http://localhost:${process.env.PORT}`)
+        console.log('Press Ctrl-C to terminate...')
+    })
+}
+
+
+main().catch(console.error)
