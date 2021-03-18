fc83aa4009a9f46fcdbb8f5a4eebd6ccf8701b27
ol222hf <ol222hf@student.lnu.se>
2021-03-18 16:31:25 +0100

    saves values in mongoDB

--- api/config/MQTT.js
+++ api/config/MQTT.js
@@ -1,17 +1,34 @@
 import mqtt from 'mqtt'
 
+import { Temperature } from '../models/Temperature.js'
+import { Humidity } from '../models/Humidity.js'
+
 export const setup = () => {
     const client = mqtt.connect(process.env.MQTT_URL)
 
-    client.on('connect', function () {
+    client.on('connect', () => {
         client.subscribe(process.env.SUB_TOPIC)
     })
 
-    client.on('message', function (topic, message) {
+    client.on('message', async (topic, message) => {
         // message is Buffer
+        const data = JSON.parse(message.toString())
         console.log(topic)
-        const msg = message.toString()
-        console.log(JSON.parse(msg))
+        console.log(data)
+
+        // Save
+        const h = new Humidity({
+            topic: topic,
+            value: data.dht11.Humidity
+        })
+        h.save()
+
+        const t = new Temperature({
+            topic: topic,
+            value: data.dht11.Temperature
+        })
+        t.save()
+
         // client.end()
     })
 }
