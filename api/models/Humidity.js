c6fb8ccba848c38e9491b0fdc8d84940b2bca01c
ol222hf <ol222hf@student.lnu.se>
2021-03-18 23:58:55 +0100

    models transform json

--- api/models/Humidity.js
+++ api/models/Humidity.js
@@ -2,11 +2,24 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const schema = new Schema({
-    topic: String,
-    value: Number,
-    unit: { type: String, default: '%' }
+    value: Number
 }, {
-    timestamps: true
+    timestamps: {
+        createdAt: true,
+        updatedAt: false
+    },
+    versionKey: false,
+    toJSON: {
+        transform: function (doc, ret) {
+            delete ret._id
+            ret.timestamp = ret.createdAt
+            delete ret.createdAt
+        }
+    }
 })
+
+schema.statics.get = async function () {
+    return this.findOne({}).sort({ createdAt: -1 }).limit(1)
+}
 
 export const Humidity = model('Humidity', schema)
