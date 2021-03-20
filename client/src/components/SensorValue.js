1c21e488cbf92d993a73d5880de332aa175b5428
ol222hf <ol222hf@student.lnu.se>
2021-03-20 13:27:49 +0100

    adding timestamp

--- client/src/components/SensorValue.js
+++ client/src/components/SensorValue.js
@@ -1,8 +1,11 @@
+import moment from 'moment'
+
 const SensorValue = (props) => {
     return (
         <div className="sensor flexCenterColumn">
             <p className="largeP">{props.value}{props.unit}</p>
             <p className="smallP">{props.name}</p>
+            <p className="smallerP">{moment(props.timestamp).format("YYYY-MM-DD HH:mm")}</p>
         </div>
     )
 }
